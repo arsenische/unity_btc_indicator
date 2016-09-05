8fb33a9f993058da45d1e7e4adbfdb5a634fcac7
Arsen Gasparyan <arsenische@gmail.com>
2016-09-05 12:11:26 +0500

    catch connection errors, log, readme

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # unity_btc_indicator
+
+Trivial bitcoin price indicator for unity.
+
+Fetches the BTCUSD price from bitcoinaverage.com every 10 seconds and displays it in tray ![screenshot with icon](http://storage1.static.itmages.com/i/16/0905/h_1473059139_6796960_32a8828b63.png).
+
+Thanks to:
+* http://candidtim.github.io/appindicator/2014/09/13/ubuntu-appindicator-step-by-step.html
+* https://jbernard.io/2010/04/16/periodic-timers-in-pygtk.html
+* https://github.com/jonwaller/weusecoinsjp-website/tree/master/remote/en.bitcoin.it/promotional_graphics
+* https://bitcoinaverage.com
--- unity_btc_indicator.py
+++ unity_btc_indicator.py
@@ -3,13 +3,14 @@
 #   https://bitcoinaverage.com
 #   http://candidtim.github.io/appindicator/2014/09/13/ubuntu-appindicator-step-by-step.html
 #   https://jbernard.io/2010/04/16/periodic-timers-in-pygtk.html
-# Todo: replace curl with some python-site 
+#   https://github.com/jonwaller/weusecoinsjp-website/tree/master/remote/en.bitcoin.it/promotional_graphics
 
-APPINDICATOR_ID = 'BtcRate'
+APPINDICATOR_ID = 'unity_btc_indicator'
 
 from gi.repository import Gtk as gtk
 from gi.repository import AppIndicator3 as appindicator
 from gi.repository import GObject as gobject
+
 
 class RateUpdater:
   def __init__(self, timeout):
@@ -20,7 +21,7 @@ class RateUpdater:
 
   def callback(self):
     self.indicator.set_status(appindicator.IndicatorStatus.ATTENTION)
-    self.indicator.set_label('$'+str(get_btc_rate()), 'hehe')
+    self.indicator.set_label('$'+str(get_btc_rate()), '$1000.00')
     self.indicator.set_menu(build_menu())
     return True
 
@@ -39,11 +40,23 @@ def build_menu():
   return menu
 
 def get_btc_rate():
-  import urllib
+
   url = "https://api.bitcoinaverage.com/ticker/global/USD/"
-  response = urllib.urlopen(url)
-  import json
-  btc_rate = json.loads(response.read())['last']
+  print "Requesting BTC price from: " + url
+
+  try:
+    import socket
+    socket.setdefaulttimeout(5)
+
+    import urllib
+    response = urllib.urlopen(url)
+    import json
+    btc_rate = json.loads(response.read())['last']
+  except Exception as e:
+    print str(e)
+    return 'n/a'
+
+  print "BTC price: $" + str(btc_rate)
   return btc_rate
 
 def quit(source):
